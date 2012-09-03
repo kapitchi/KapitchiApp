13ffde03973a8d4cf51f209e9cfa13b00d33248e
Matus Zeman <mz@kapitchi.com>
2012-09-03 13:32:36 +0100

    kapitchi-* routes renamed to names without kapitchi- prefix

--- config/module.config.php
+++ config/module.config.php
@@ -24,7 +24,7 @@ return array(
     ),
     'router' => array(
         'routes' => array(
-            'kapitchi-app' => array(
+            'app' => array(
                 'type'    => 'Literal',
                 'options' => array(
                     'route'    => '/app',
